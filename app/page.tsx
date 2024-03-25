8a82064f469ae5cb986b7fc85a26a70e74104b26
Surya <suryakant.subudhi@outlook.com>
2024-03-25 12:41:11 +0530

    auth mod

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Hello World!
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
