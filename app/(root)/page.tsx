5e35b81148596c73ed1de30a8c13a08fbe9ae56c
Surya <suryakant.subudhi@outlook.com>
2024-03-27 14:37:07 +0530

    🪄Added theme toggle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/modeToggle";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/themeProvider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -18,8 +19,12 @@ export default function RootLayout({
   return (
     /** Mount Clerk provider here */
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="harmonyDark">
+              {children}
+            </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
