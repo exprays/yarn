a075bfbf1834416afe2fc9eff420f558cce3554a
Surya <suryakant.subudhi@outlook.com>
2024-04-05 15:08:32 +0530

    utils to handle profiles

--- app/(onboarding)/page.tsx
+++ app/(onboarding)/page.tsx
@@ -1,4 +1,24 @@
-const OnboardingPage = () => {
+import { db } from "@/lib/db";
+import { initProfile } from "@/utils/initProfile";
+import { redirect } from "next/navigation";
+
+const OnboardingPage = async () => {
+    const profile = await initProfile();
+
+    const server =  await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
+
     return ( 
         <div>
             Create a Server
---
+++ utils/initProfile.ts
@@ -0,0 +1,34 @@
+// checks for profiles
+
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const initProfile = async () => {
+    const user = await currentUser();
+
+    if (!user) {
+        return redirectToSignIn();
+    }
+
+    const profile = await db.profile.findUnique({
+        where: {
+            userId: user.id
+        }
+    });
+
+    if (profile) {
+        return profile;
+    }
+
+    const newProfile =  await db.profile.create({
+        data: {
+            userId: user.id,
+            name:`${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    });
+
+    return newProfile;
+}
