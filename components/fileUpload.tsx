82f7742100555afb6881de496177f235ef85f23d
Surya <suryakant.subudhi@outlook.com>
2024-05-14 10:20:51 +0530

    improvements

--- components/fileUpload.tsx
+++ components/fileUpload.tsx
@@ -3,6 +3,9 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
+import { X } from "lucide-react";
+import Image from "next/image";
+
 interface FileUploadProps {
     onChange: (url?: string) => void;
     value: string;
@@ -14,6 +17,22 @@ export const FileUpload = ({
     value,
     endpoint
 }: FileUploadProps) => {
+
+    const fileType = value?.split(".").pop();
+
+    if (value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image 
+                    fill
+                    src={value}
+                    alt="upload"
+                    className="rounded-full"
+                />                
+            </div>
+        )
+    }
+
     return (
         <UploadDropzone 
             endpoint={endpoint}
