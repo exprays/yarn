ad717a910e2addd778a8716e6fdac03641c18d14
Surya <suryakant.subudhi@outlook.com>
2024-05-15 08:26:21 +0530

    UX enhancement

--- components/fileUpload.tsx
+++ components/fileUpload.tsx
@@ -28,7 +28,10 @@ export const FileUpload = ({
                     src={value}
                     alt="upload"
                     className="rounded-full"
-                />                
+                />
+                <button onClick={() => onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
+                    <X className="h-4 w-4"/>
+                </button>                
             </div>
         )
     }
