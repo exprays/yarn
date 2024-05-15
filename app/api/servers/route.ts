ec074526c0f0db2ecb33bd00e33bf30dc255ec08
Surya <suryakant.subudhi@outlook.com>
2024-05-15 09:45:38 +0530

    Completed server creation API

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,5 +1,10 @@
-import { currentProfile } from "@/utils/currentProfile";
+import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
+
+import { MemberRole } from "@prisma/client";
+
+import { currentProfile } from "@/utils/currentProfile";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
     try {
@@ -9,6 +14,29 @@ export async function POST(req: Request) {
         if (!profile) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+
+        // Create the server in database
+        const server = await db.server.create({
+            data: {
+                profileId: profile.id,
+                name,
+                imageUrl,
+                inviteCode: uuidv4(),
+                channels: {
+                    create: [
+                        { name: "general", profileId: profile.id }
+                    ]
+                },
+                members: {
+                    create: [
+                        { profileId: profile.id, role: MemberRole.ADMIN }
+                    ]
+                }
+            }
+        });
+
+        return NextResponse.json(server);
+        //returns JSON object
 
     } catch (error) {
         console.log("[SERVERS_POST_ERROR]", error);
