404ab4db7a18f0816b0c7b7d2a234a90e995dab0
Surya <suryakant.subudhi@outlook.com>
2024-05-17 08:49:09 +0530

    extended modalStore for settings modal

--- hooks/useModalStore.ts
+++ hooks/useModalStore.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite";
+export type ModalType = "createServer" | "invite" | "serverSetting";
 
 interface ModalData {
     server?: Server
