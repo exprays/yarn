9bc921491d144105ca123fbfc3f7d713dafb98f3
Surya <suryakant.subudhi@outlook.com>
2024-05-21 11:10:51 +0530

    💥Completed server section

--- components/elements/server/section.tsx
+++ components/elements/server/section.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { ActionTooltip } from "@/components/actions/actionTooltip";
+import { useModal } from "@/hooks/useModalStore";
 import { ServerTypes } from "@/types/serverTypes";
 import { ChannelType, MemberRole } from "@prisma/client";
-import { Plus } from "lucide-react";
+import { Plus, Settings } from "lucide-react";
 
 interface ServerSectionProps {
     label: string;
@@ -20,6 +21,9 @@ export const ServerSection = ({
     channelType,
     server
 }: ServerSectionProps) => {
+
+    const { onOpen } = useModal();
+
     return (
         <div className="flex items-center justify-between py-2">
             <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
@@ -27,8 +31,15 @@ export const ServerSection = ({
             </p>
             {role !== MemberRole.GUEST && sectionType === "channels" && (
                 <ActionTooltip label="Create Channel" side="top">
-                    <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+                    <button onClick={() => onOpen("createChannel")} className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
                         <Plus className="h-4 w-4"/>
+                    </button>
+                </ActionTooltip>
+            )}
+            {role === MemberRole.ADMIN && sectionType === "members" && (
+                <ActionTooltip label="Manage Members" side="top">
+                    <button onClick={() => onOpen("members", { server })} className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
+                        <Settings className="h-4 w-4"/>
                     </button>
                 </ActionTooltip>
             )}
