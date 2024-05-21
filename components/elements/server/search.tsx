0f96a8e6da1e32f8e4d5f62ccd44ec13b38b640f
Surya <suryakant.subudhi@outlook.com>
2024-05-21 10:13:17 +0530

    🧪Added command functionality

--- components/elements/server/search.tsx
+++ components/elements/server/search.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Command, Search } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface ServerSearchProps {
     data: {
@@ -14,9 +18,25 @@ interface ServerSearchProps {
 
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                setOpen((open) => !open) //opens the search box by pressing ctrl + K
+            } //debug for windows edge
+        }
+
+        document.addEventListener("keydown", down);
+        return () => document.removeEventListener("keydown", down);
+
+    }, [])
+
     return (
         <>
-            <button className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+            <button onClick={() => setOpen(true)} className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
                 <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400"/>
                 <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
                     Search
@@ -25,6 +45,32 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
                     <Command className="h-3 w-3"/><span className="text-small">+ K</span>
                 </kbd>
             </button>
+
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Search all channels and members"/>
+                <CommandList>
+                    <CommandEmpty>
+                        No results found
+                    </CommandEmpty>
+                    {data.map(({ label, type, data }) => {
+                        if(!data?.length) return null;
+
+                        return (
+                            <CommandGroup key={label} heading={label}>
+                                {data?.map(({ id, icon, name }) => {
+                                    return (
+                                        <CommandItem key={id}>
+                                            {icon}
+                                            <span>{name}</span>
+                                        </CommandItem>
+                                    )
+                                })}
+                            </CommandGroup>
+                        )
+                    })}
+                </CommandList>
+            </CommandDialog>
+
         </>
     )
 }
