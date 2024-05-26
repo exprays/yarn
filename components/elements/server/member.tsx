d8857d041ff296430baffa2d9f5d64a48f2f9e46
Surya <suryakant.subudhi@outlook.com>
2024-05-26 10:30:44 +0530

    typo fix

--- components/elements/server/member.tsx
+++ components/elements/server/member.tsx
@@ -36,7 +36,7 @@ export const ServerMember = ({
             <UserAvatar src={member.profile.imageUrl} className="h-8 w-8 md:h-8 md:w-8" />
             <p className={cn(
                 "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
             )}>
                 {member.profile.name}
             </p>
