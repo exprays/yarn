5089b57325fb2bac3d165c5f9bb13682eb7276b5
Surya <suryakant.subudhi@outlook.com>
2024-05-26 19:56:52 +0530

    📱added chat header component

--- app/(root)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(root)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,53 @@
-const ChannelIdPage = () => {
+import ChatHeader from "@/components/chat/header";
+import { db } from "@/lib/db";
+import { currentProfile } from "@/utils/currentProfile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+
+    const profile = await currentProfile();
+
+    if(!profile) {
+        return redirectToSignIn();
+    }
+
+    //fetch respective channel
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId
+        }
+    });
+
+    //find the members in the server
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id
+        }
+    });
+
+    if(!channel || !member) {
+        redirect("/");
+    }
+
     return ( 
-        <div>
-            Channel id
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader 
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
      );
 }
