91ceaccd43746cd85b91edd0c7fb344a4b4341f4
Surya <suryakant.subudhi@outlook.com>
2024-05-26 11:48:51 +0530

    🪄Added default general channel redirect

--- app/(root)/(routes)/servers/[serverId]/page.tsx
+++ app/(root)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,53 @@
-const ServerPage = () => {
-    return ( 
-        <div>
-            Server ID page
-        </div>
-     );
+import { db } from "@/lib/db";
+import { currentProfile } from "@/utils/currentProfile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+}
+
+
+const ServerPage = async ({
+    params
+}: ServerIdPageProps) => {
+
+    const profile = await currentProfile();
+
+    if(!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                }
+            }
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            }
+        }
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if(initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
  
 export default ServerPage;
