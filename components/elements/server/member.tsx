326ac8853ebac0b34188c60d310cc2259dc65e82
Surya <suryakant.subudhi@outlook.com>
2024-05-26 10:32:51 +0530

    member redirect

--- components/elements/server/member.tsx
+++ components/elements/server/member.tsx
@@ -28,8 +28,13 @@ export const ServerMember = ({
 
     const icon = roleIconMap[member.role];
 
+    //redirect
+    const onClick = () => {
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    }
+
     return (
-        <button className={cn(
+        <button onClick={onClick} className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
         )}>
